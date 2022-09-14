4b52a72a2743bb95a5f5f85b1d6f0d8b6ec09fdd
Nakajjang <113503411+Nakajjang@users.noreply.github.com>
2022-09-14 16:57:07 +0900

    Add files via upload

---
+++ eclipse-workspace/hk220914/src/SmClass.java
@@ -0,0 +1,9 @@
+
+public class SmClass {
+
+	public static void main(String[] args) {
+		System.out.println("rhee soong moo");
+
+	}
+
+}
---
+++ eclipse-workspace/hk220914/src/hk220914/TestClass.java
@@ -0,0 +1,13 @@
+package hk220914;
+
+public class TestClass {
+
+	
+	public static void main(String [] args) {
+		System.out.println("2022.09.14");
+	}
+	
+	
+	
+	
+}
